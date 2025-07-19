84c89b15d698ffee9ba0f2095b44726592aab5fe
PThorpe92 <preston@turso.tech>
2025-07-19 15:30:26 -0400

    Hold and ensure release of proper locks if we trunc the db file post-checkpoint

--- core/storage/pager.rs
+++ core/storage/pager.rs
@@ -286,7 +286,7 @@ pub struct Pager {
     free_page_state: RefCell<FreePageState>,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 /// The status of the current cache flush.
 pub enum PagerCommitResult {
     /// The WAL was written to disk and fsynced.
@@ -1064,7 +1064,7 @@ impl Pager {
             return Ok(CheckpointResult::default());
         }
 
-        let checkpoint_result = self.io.block(|| {
+        let mut checkpoint_result = self.io.block(|| {
             self.wal
                 .borrow_mut()
                 .checkpoint(self, Rc::new(RefCell::new(0)), mode)
@@ -1090,6 +1090,7 @@ impl Pager {
                     tracing::trace!("Database file syncd after truncation");
                 }))?;
             }
+            checkpoint_result.release_guard();
         }
 
         // TODO: only clear cache of things that are really invalidated
